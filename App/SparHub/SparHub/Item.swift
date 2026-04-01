8d6be871093b155961f14858f682ce734a49bf3d
Zasenko <dmitry.zasenko@gmail.com>
2026-04-01 13:58:31 +0200

    add new project

---
+++ App/SparHub/SparHub/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SparHub
+//
+//  Created by Dmitry Zasenko on 01.04.26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
