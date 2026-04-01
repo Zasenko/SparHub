71a88c7289cd9f99fc11331c7249855ce9f0fc01
Zasenko <dmitry.zasenko@gmail.com>
2026-04-01 14:26:59 +0200

    add tabview + router

--- App/SparHub/SparHub/ContentView.swift
+++ App/SparHub/SparHub/ContentView.swift
@@ -7,13 +7,60 @@
 
 import SwiftUI
 
+enum Router {
+    case home
+    case user
+    case search
+    case add
+}
+
 struct ContentView: View {
 
+    @State private var router: Router = .home
+        
     var body: some View {
-        NavigationStack {
-            VStack {
-                
+        ZStack {
+            switch router {
+            case .home:
+                Color.blue
+            case .search:
+                Color.yellow
+            case .user:
+                Color.green
+            case .add:
+                Color.orange
             }
+            HStack(spacing: 40) {
+                TabButton(router: $router, route: .home, img: AppImages.iconHome)
+                TabButton(router: $router, route: .search, img: AppImages.iconSearch)
+                TabButton(router: $router, route: .add, img: AppImages.iconPlus)
+                TabButton(router: $router, route: .user, img: AppImages.iconUser)
+            }
+            .padding()
+            .padding(.horizontal)
+            .background(.red)
+            .clipShape(Capsule())
+            .padding(.horizontal)
+            .frame(maxHeight: .infinity, alignment: .bottom)
+        }
+        .animation(.smooth, value: router)
+    }
+}
+
+struct TabButton: View {
+    
+    @Binding var router: Router
+
+    let route: Router
+    let img: Image
+    
+    var body: some View {
+        Button {
+            router = route
+        } label: {
+            img
+                .bold()
+                .foregroundStyle(router == route ? .black : .secondary)
         }
     }
 }
---
+++ App/SparHub/SparHub/Resources/AppImages.swift
@@ -0,0 +1,15 @@
+//
+//  AppImages.swift
+//  SparHub
+//
+//  Created by Dmitry Zasenko on 01.04.26.
+//
+
+import SwiftUI
+
+struct AppImages {
+    static let iconPlus = Image(systemName: "plus")
+    static let iconHome = Image(systemName: "house.fill")
+    static let iconSearch = Image(systemName: "magnifyingglass")
+    static let iconUser = Image(systemName: "person.fill")
+}
